1e72ad494a4eff2db17c5367c949a24316801e9a
Dipu Chowdhury <dipuchowdhury@dipus-mbp.myfiosgateway.com>
2024-03-30 14:38:10 -0400

    added info

--- subnet.py
+++ subnet.py
@@ -1,65 +1,65 @@
 #enter your unique code
-uniquecode = 123
+uniquecode = 69733
 
-#problem 1
-p1Network = ''
-p1Broadcast = ''
-p1First = ''
-p1Last =  ''
+# Problem 1
+p1Network = '227.233.111.128'
+p1Broadcast = '227.233.111.135'
+p1First = '227.233.111.129'
+p1Last = '227.233.111.134'
 
-#problem 2
-p2Network = ''
-p2Broadcast = ''
-p2First = ''
-p2Last = ''
+# Problem 2
+p2Network = '138.218.73.160'
+p2Broadcast = '138.218.73.191'
+p2First = '138.218.73.161'
+p2Last = '138.218.73.190'
 
-#problem 3
-p3Network = ''
-p3Broadcast = ''
-p3First = ''
-p3Last = ''
+# Problem 3
+p3Network = '49.203.35.0'
+p3Broadcast = '49.203.35.255'
+p3First = '49.203.35.1'
+p3Last = '49.203.35.254'
 
-#problem 4
-p4Network = ''
-p4Broadcast = ''
-p4First = ''
-p4Last = ''
+# Problem 4
+p4Network = '216.188.252.0'
+p4Broadcast = '216.188.252.31'
+p4First = '216.188.252.1'
+p4Last = '216.188.252.30'
 
-#problem 5
-p5Network = ''
-p5Broadcast = ''
-p5First = ''
-p5Last = ''
+# Problem 5
+p5Network = '127.174.214.0'
+p5Broadcast = '127.174.214.127'
+p5First = '127.174.214.1'
+p5Last = '127.174.214.126'
 
-#problem 6
-p6Network = ''
-p6Broadcast = ''
-p6First = ''
-p6Last =  ''
+# Problem 6
+p6Network = '38.159.176.80'
+p6Broadcast = '38.159.176.95'
+p6First = '38.159.176.81'
+p6Last = '38.159.176.94'
 
-#problem 7
-p7Network = ''
-p7Broadcast = ''
-p7First = ''
-p7Last = ''
+# Problem 7
+p7Network = '205.144.138.64'
+p7Broadcast = '205.144.138.127'
+p7First = '205.144.138.65'
+p7Last = '205.144.138.126'
 
-#problem 8
-p8Network = ''
-p8Broadcast = ''
-p8First = ''
-p8Last = ''
+# Problem 8
+p8Network = '116.129.99.168'
+p8Broadcast = '116.129.99.175'
+p8First = '116.129.99.169'
+p8Last = '116.129.99.174'
 
-#problem 9
-p9Network = ''
-p9Broadcast = ''
-p9First = ''
-p9Last = ''
+# Problem 9
+p9Network = '27.114.61.192'
+p9Broadcast = '27.114.61.223'
+p9First = '27.114.61.193'
+p9Last = '27.114.61.222'
 
-#problem 10
-p10Network = ''
-p10Broadcast = ''
-p10First = ''
-p10Last = ''
+# Problem 10
+p10Network = '194.99.23.0'
+p10Broadcast = '194.99.23.255'
+p10First = '194.99.23.1'
+p10Last = '194.99.23.254'
 
 
 """
